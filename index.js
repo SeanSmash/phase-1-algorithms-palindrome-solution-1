2cb94e535b636e253b0ea91063093a7b56efba07
Sean Sarokas <sps4462@gmail.com>
2022-05-19 09:23:09 -0400

    commit tree 66b9fe5de0ae71b903b5fdd7897e1c5961369f1a

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word)
+  return (word === reversedWord)
 }
 
+function reverse(word){
+  return word.split('').reverse().join('')
+}
 /* 
   Add your pseudocode here
+  word.reverse()
+  if(word === reversedword){
+    return true
+  } else {
+    return false
+  }
 */
 
 /*
   Add written explanation of your solution here
+  a function that takes a word and returns true if the word reads the
+  same forwards as it does backwards, and returns false otherwise
 */
 
 // You can run `node index.js` to view these console logs
